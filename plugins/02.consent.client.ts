39ff4117b7be0b4347bda97edef0792e5e6c2481
Fusion Axi Dev Team <dev@fusionaxi.com>
2024-10-22 21:44:00 +0800

    Fix the domain cmp

--- plugins/02.consent.client.ts
+++ plugins/02.consent.client.ts
@@ -5,7 +5,7 @@ export default defineNuxtPlugin(() => {
         src: `https://cdn.consentmanager.net/delivery/autoblocking/17db6d5c11b48.js`,
         type: 'text/javascript',
         'data-cmp-ab': '1',
-        'data-cmp-host': 'c.delivery.consentmanager.ne',
+        'data-cmp-host': 'c.delivery.consentmanager.net',
         'data-cmp-cdn': 'cdn.consentmanager.net',
         'data-cmp-codesrc': '16'
       }
